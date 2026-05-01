a64570f7b4df109cae0001ef847c6b09161bace4
Rizky Afandi <afandirizky121@gmail.com>
2026-05-01 23:08:14 +0700

    update teks footer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 // Import semua halaman dari folder pagas
 import 'pagas/splash_screen.dart';
-import 'pagas/login_page.dart';        // <--- Pastikan ini sudah di-import
+import 'pagas/login_page.dart'; // <--- Pastikan ini sudah di-import
 import 'pagas/auth/register_page.dart';
 import 'pagas/auth/otp_page.dart';
 import 'pagas/profile_setup_page.dart';
@@ -14,11 +14,11 @@ import 'pagas/profile_page.dart';
 import 'pagas/reward_referral_page.dart';
 import 'pagas/reward_riwayat_page.dart';
 import 'pagas/reward_tukar_page.dart';
-import 'pagas/challenge_aktif_page.dart';
-import 'pagas/challenge_tersedia_page.dart';
-import 'pagas/challenge_selesai_page.dart';
+import 'pagas/challenge/challenge_aktif_page.dart';
+import 'pagas/challenge/challenge_tersedia_page.dart';
+import 'pagas/challenge/challenge_selesai_page.dart';
 import 'pagas/setor_utama_page.dart';
-import 'pagas/setor_jemput_page.dart'; 
+import 'pagas/setor_jemput_page.dart';
 import 'pagas/setor_drop_point_page.dart';
 
 void main() {
@@ -40,18 +40,19 @@ class MyApp extends StatelessWidget {
           primary: const Color(0xFF1B3022),
         ),
         useMaterial3: true, // Biar tampilan tombol & input lebih kekinian
-        fontFamily: 'Inter', 
+        fontFamily: 'Inter',
       ),
-      
+
       // Halaman awal tetap Splash Screen
       home: const SplashScreen(),
 
       // Peta jalan (Navigasi) aplikasi kamu
       routes: {
-        '/login': (context) => const LoginPage(),         // Alamat Login
-        '/register': (context) => const RegisterPage(),   // Alamat Daftar
-        '/otp': (context) => const OtpPage(),             // Alamat Verifikasi OTP
-        '/profile_setup': (context) => const ProfileSetupPage(), // Alamat Lengkapi Profil
+        '/login': (context) => const LoginPage(), // Alamat Login
+        '/register': (context) => const RegisterPage(), // Alamat Daftar
+        '/otp': (context) => const OtpPage(), // Alamat Verifikasi OTP
+        '/profile_setup': (context) =>
+            const ProfileSetupPage(), // Alamat Lengkapi Profil
         '/onboarding_one': (context) => const OnboardingOne(),
         '/onboarding_two': (context) => const OnboardingTwo(),
         '/onboarding_three': (context) => const OnboardingThree(),
@@ -66,7 +67,7 @@ class MyApp extends StatelessWidget {
         '/challenge_selesai': (context) => const ChallengeSelesaiPage(),
         '/setor_utama': (context) => const SetorUtamaPage(),
         '/setor_jemput': (context) => const SetorJemputPage(),
-         '/setor_drop_point': (context) => const SetorDropPointPage(),
+        '/setor_drop_point': (context) => const SetorDropPointPage(),
       },
     );
   }
